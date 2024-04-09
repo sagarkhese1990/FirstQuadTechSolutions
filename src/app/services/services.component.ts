3ff7a3ca1b21ff554e081257bc183cd56765fc74
Vinod Patne <vinod.patne@gmail.com>
2024-04-09 16:54:56 +0530

    Updated Services and description

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -10,28 +10,28 @@ import { Router } from '@angular/router';
 export class ServicesComponent {
   ourServices: Service[] = [
     {
-      name: 'Software Solutions',
+      name: 'Software Engineering Services',
       description : 'While correct use of technology has long been an important factor in business success, the need for relevant IT solutions is witnessing an unprecedented growth in today’s market.',
       icon: 'lightbulb-fill',
       routePath: 'services/it-solutions'
     },
     {
-      name: 'Professional Training',
-      description : 'We provide professional training to working professionals as well as graduates to gain expertise in specific areas, and enable them to grab opportunities aligned to their career aspirations.',
+      name: 'Professional Trainings',
+      description : 'We provide professional training to working professionals as well as graduates to gain expertise in specific areas and enable them to seize opportunities aligned with their career aspirations. Additionally, we offer legitimate employment and exposure to the real-world industry.',
       icon: 'person-bounding-box',
       routePath: 'services/professional-training'
     },
     {
-      name: 'Employment Solutions',
-      description : 'We provide professional training to graduates along with legal employment, and exposure to real world industry. We assist them to foster their skills and experience to match the industry demand.',
-      icon: 'person-raised-hand',
-      routePath: 'services/employment-solutions'
-    },
-    {
-      name: 'Staffing Services',
-      description : 'Our talent acquisition team specializes in identifying, sourcing, and recruiting top talent for companies. Our aim is to assist companies in staff augmentation through managed services, allowing them to concentrate on their core business.',
+      name: 'Contract Staffing',
+      description : 'Our commitment lies in assisting companies with staff augmentation through managed services tailored to their unique requirements and organizational culture. By doing so, we enable companies to focus on their core business, driving growth and success. ',
       icon: 'people-fill',
       routePath: 'services/contract-staffing'
+    },
+    {
+      name: 'Staffing Agency',
+      description : 'Our talent acquisition team excels at identifying, sourcing, and recruiting top talent for companies. We are specialized in matching skill sets and experiences of job seekers with the requirements of available job positions.',
+      icon: 'person-raised-hand',
+      routePath: 'services/employment-solutions'
     }
   ];
   constructor(private router: Router){}
